9e70db19b01db1377e1511eb1e2dc383fc44bf7d
Sushant-4444 <arorasushant4444@gmail.com>
2025-08-01 21:55:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,11 +45,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
-app.use(cors({
-  origin: 'http://localhost:5173', // or '*'
-  credentials: true
-}));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'documents')));
 app.use(express.json());
 app.use("/auth", authroutes);
